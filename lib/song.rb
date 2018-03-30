12721a85c9613716bf1f3f138feab8f4b3c3de29
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-03-30 21:52:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,10 +25,8 @@ class Song
   end
 
   def self.create_by_name(name)
-    song = self.new
-    song.name = name
+    song = self.new_by_name(name)
     song.save
-    song
   end
 
   def self.find_by_name(name)
