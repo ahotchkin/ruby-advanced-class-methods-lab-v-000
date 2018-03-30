0405066ed05cbc3086397ee2913fadfd7c5e1ea2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-03-30 21:16:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   def self.new_from_filename(filename)
     song = self.new
     parsed_name = filename.split(/\ - |\.mp3/)
-    song.artist_name = parsed_name[0]
+    artist_name = parsed_name[0]
     name = parsed_name[1]
 
   end
